86b064e563c4ee06a720d3cf3a6ddb57e8568dd4
venugopal <141745939+venumachina@users.noreply.github.com>
2023-08-17 16:20:55 +0530

    Add files via upload

---
+++ Addition1.java
@@ -0,0 +1,11 @@
+package day1;
+ class Addition1 {
+ int a;int b;int c;
+ a=20;
+ b=30;
+ c=a+b;
+ public static void main(string[]args) {
+
+ System.out.println("addition of two numbers"+c);
+ }
+}
